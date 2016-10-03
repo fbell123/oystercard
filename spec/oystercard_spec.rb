22661d6f43b69df6f8058cf3121bfa6857bb4634
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-10-03 15:20:57 +0100

    deduct method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,10 +10,17 @@ describe OysterCard do
   it "Oyster card can be topped up" do
     expect(subject).to respond_to(:top_up).with(1).argument
   end
+
   it "raises error if over max" do
     max = OysterCard::MAX
     subject.top_up(max)
     expect{subject.top_up 1 }.to raise_error("TOO MUCH! #{max} is the max")
+  end
+
+  it 'deduct journey from balance' do
+    subject.top_up(2)
+    subject.deduct(1)
+    expect(subject.balance).to eq(1) 
   end
 
 end
